542655b67ec56c2f81430b963e9b6c5ef37f628b
Yerimimd07 <yerimi.miranda@campusucc.edu.co>
2026-09-05 01:35:30 +0000

    Potencia 2 arreglo b

--- Semana1/Ejecutar.java
+++ Semana1/Ejecutar.java
@@ -4,19 +4,35 @@ import javax.swing.plaf.synth.SynthOptionPaneUI;
 
 public class Ejecutar {
     public static void main(String[] args) {
-        //Declaracion del arreglo-array-vector (Forma 1)
-        int[] a={5, 16, 8, 9, 10, 25, 19, 3, 6, 22, 2};
-       
-        for(int i = 0; i < a.length; i++){
-        System.out.println("a[" + i + "]=" + a[i]);
+        // Declaracion del arreglo-array-vector (Forma 1)
+        int[] a = { 5, 16, 8, 9, 10, 25, 19, 3, 6, 22, 2 };
+
+        for (int i = 0; i < a.length; i++) {
+            System.out.println("a[" + i + "]=" + a[i]);
+
         }
-        //Sumar todos los elementos del arreglo a
+        // Sumar todos los elementos del arreglo a
         int suma = 0;
-        for(int i= 0; i < a.length; i++){
+        for (int i = 0; i < a.length; i++) {
             suma += a[i];
         }
-
+        System.out.println();
         System.out.println("La suma de los elementos del arreglo a: " + suma);
+        System.out.println();
+
+        // Declaracion del arreglo-array-vector (Forma 2)
+
+        int[] b = new int[11];
+        int min = 1;
+        int max = 30;
+        for (int i = 0; i < a.length; i++) {
+            // llenar el arreglo b con numero aleatorios entre 1(minimo) y 50(Maximo)
+            b[i] = (int) (Math.random() * (max - min - 1) + min);
+        }
+        //Mostrar arreglo b --> Elevar al cuadrado cada posicion 
+        for (int i = 0; i < b.length; i++) {
+            System.out.println("b[" + i + "]=" + b[i] + " Potencia 2 = " + Math.pow(b[i],2));
+        }
 
     }
 }
