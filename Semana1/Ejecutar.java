fe3b59bf931125a5b7e84ce91eed65914e830a41
Yerimimd07 <yerimi.miranda@campusucc.edu.co>
2026-09-05 01:09:26 +0000

    Suma de todos los elementos

--- Semana1/Ejecutar.java
+++ Semana1/Ejecutar.java
@@ -1,7 +1,22 @@
 package Semana1;
 
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+
 public class Ejecutar {
     public static void main(String[] args) {
-        System.out.println("Estructura de datos UCC");
+        //Declaracion del arreglo-array-vector (Forma 1)
+        int[] a={5, 16, 8, 9, 10, 25, 19, 3, 6, 22, 2};
+       
+        for(int i = 0; i < a.length; i++){
+        System.out.println("a[" + i + "]=" + a[i]);
+        }
+        //Sumar todos los elementos del arreglo a
+        int suma = 0;
+        for(int i= 0; i < a.length; i++){
+            suma += a[i];
+        }
+
+        System.out.println("La suma de los elementos del arreglo a: " + suma);
+
     }
 }
